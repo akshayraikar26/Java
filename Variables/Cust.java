6742f27233a603ff3e0db117f0093629f89f1200
Akshay Ullas Shet <128679446+akshayraikar26@users.noreply.github.com>
2023-12-03 21:21:58 +0530

    Add files via upload

---
+++ Variables/Cust.java
@@ -0,0 +1,16 @@
+class Cust
+{
+	public static void main(String[] args) 
+	{
+     String custname="Rohith";
+     String mall_name="LuLu";
+     String prd_name="T-Shirt";
+     double price=4999.56;
+     char grade='A';
+        System.out.println("Custname is "+custname);
+        System.out.println("Mall Name is "+mall_name);
+        System.out.println("Product Name is "+prd_name);
+        System.out.println("Price is "+price);
+        System.out.println("Product grade is "+grade);
+	}
+}
